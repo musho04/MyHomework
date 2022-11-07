dac1d47f7eba221d2c777aaabc49add27185b1be
Mushegh <mushegh.esayan.im@gmail.com>
2022-11-07 20:13:14 +0400

    Homework 07.11.2022

---
+++ cpp/day1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+//added a and b
+int add ( int a, int b ) {
+    std::cout << "Sum: " << a + b << std::endl;
+    return 0;
+};
+
+//Max and Min
+int maxmin ( int a , int b ){
+    //max a, min b
+    if ( a > b ){
+        std::cout << "Min: " << b << std::endl;
+        std::cout << "Max: " << a << std::endl;
+    }else if ( a < b ){ //Max b, min a
+        std::cout << "Min: " << a << std::endl;
+        std::cout << "Max: " << b << std::endl;
+    }else {
+        std::cout << "a = b" << std::endl; //If a = b
+    }
+    return 0;
+};
+
+int main()
+{
+    //Declared a and b
+    int a, b;
+    //Import value a
+    std::cout << "Insert a: ";
+    std::cin >> a;
+    //Import vale b
+    std::cout << "Insert b: ";
+    std::cin >> b;
+    //function call
+    maxmin(a, b);
+    add(a, b);
+    return 0;
+}
+
